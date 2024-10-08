5d17088524fedd790356874ed4ecd0f90ce32439
Mark Sloane <mdsloane@gmail.com>
2024-10-07 22:19:10 -0700

    Add user registration logic and .gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
-app = Flask(__name__)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 
-from app import routes
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your_secret_key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-from app import app
+    # Initialize extensions with the app
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
 
-@app.route('/')
-def home():
-    return "Welcome to the Wellness App!"
+    from app import routes
+    app.register_blueprint(routes.bp)
 
+    return app
